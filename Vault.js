f10f469c2b7674f16e8c08c81f2600db989cbf26
Jdogg28 <joevanpelt0@gmail.com>
2024-09-16 19:43:50 -0500

    first commit

---
+++ Vault.js
@@ -0,0 +1,13 @@
+
+console.log("Hello world")
+// createstring
+const string="You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+// assign three variables
+const num1=12-2
+const num2=20+20
+const num3=3*13
+// dialog box
+// 12 subtracted by 2
+// twenty plus twenty
+// 3 multiplied by 13
+alert(string+num1+", "+num2+ ", "+num3)
